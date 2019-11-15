96cf3cac6a1f423292b6c8098c04fa934a8f1170
Tomohiro <tomo.nz3156@gmail.com>
2019-11-16 00:05:54 +0900

    :wrench: Add GoogleFonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,18 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap&subset=japanese'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Nunito:700&display=swap'
+      }
+    ]
   },
 
   /*
