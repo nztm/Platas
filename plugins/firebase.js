d1d889f899a241e1ac6d044e67c94577e910ca14
Tomohiro <tomo.nz3156@gmail.com>
2019-12-06 10:28:52 +0900

    :ambulance: Fix firebase warning

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+// import 'firebase/auth'
 
 const config = {
   projectId: process.env.FIREBASE_PROJECT_ID
