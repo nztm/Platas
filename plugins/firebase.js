b5015abbc6a5bb9ef8e3c362487a9ea04053979a
Tomohiro <tomo.nz3156@gmail.com>
2019-12-09 16:05:27 +0900

    :wrench: Update firebase config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/firestore'
 // import 'firebase/auth'
 
 const config = {
-  projectId: process.env.FIREBASE_PROJECT_ID
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTH,
+  databaseURL: process.env.FIREBASE_DB,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 if (!firebase.apps.length) {
