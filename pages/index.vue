8c98f0fc28b5a07f816c098917ae498e2a5bf70c
Tomohiro <tomo.nz3156@gmail.com>
2019-12-14 22:07:19 +0900

    :construction: Add user sign-in

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,22 @@
 </template>
 
 <script>
+// Components
 import Counter from '~/components/Counter.vue'
 import Object from '~/components/Object.vue'
 import Button from '~/components/Button.vue'
+
+// Plugins
+import firebase from '~/plugins/firebase'
 
 export default {
   components: {
     Counter,
     Object,
     Button
+  },
+  created() {
+    firebase.auth().signInAnonymously()
   }
 }
 </script>
