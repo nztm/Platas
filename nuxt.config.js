38c5b9b1788c46c6d5690036919e4833b4a8b1c3
Tomohiro <tomo.nz3156@gmail.com>
2020-01-24 21:18:43 +0900

    :wrench: Update viewport settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,11 @@ export default {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no'
+      },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
